54e4103cc41b72509d74aca74a39a2f2c080b1d8
damianGG <damiangolon@gmail.com>
2024-10-18 14:55:45 +0200

    improvmet download links

--- src/app/aktualnosci/[slug]/page.tsx
+++ src/app/aktualnosci/[slug]/page.tsx
@@ -35,12 +35,7 @@ export default async function BlogDetailsTemplate({ params }: { params: { slug: 
 
     const photoURL = data.attributes.zdjecie?.data?.attributes?.url;
     const tekstPlikDoPobrania = data.attributes?.TesktPlikDoPobrania || '';
-    const downloadURL = data.attributes.PlikDoPobrania?.data?.[0]?.attributes?.url;
-
-    console.log('downloadURL', downloadURL);
-
-
-
+    const downloadURL = data.attributes.PlikDoPobrania?.data?.attributes?.url;
 
     return (
         <>
